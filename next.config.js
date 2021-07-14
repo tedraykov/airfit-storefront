f05c6c053828b16b48f1d3886bb6d89ca5ee82f1
tedraykov <tedraykov@gmail.com>
2021-07-14 22:03:19 +0300

    Make sidebar in product page sticky on desktop

--- next.config.js
+++ next.config.js
@@ -17,8 +17,8 @@ module.exports = withCommerceConfig({
   },
   commerce,
   i18n: {
-    locales: ['en-US', 'es'],
-    defaultLocale: 'en-US',
+    locales: ['bg-BG'],
+    defaultLocale: 'bg-BG',
   },
   rewrites() {
     return [
